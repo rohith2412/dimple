fd4e838e70eee9d5430b8976d14eeaded4a19ace
rohith <rohithra75@gmail.com>
2025-07-02 04:11:54 -0400

    added ui

--- app/client/myProfile/page.jsx
+++ app/client/myProfile/page.jsx
@@ -3,9 +3,7 @@
 import Background from "@/app/components/Background";
 import Gears from "@/app/components/Gear";
 import MainProfileView from "@/app/components/MainProfileView";
-import { Mysettings } from "@/app/components/Mysettings";
 import Navbar from "@/app/components/Navbar";
-import ViewPhoto from "@/app/components/ViewPhoto";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import React, { useEffect } from "react";
--- app/components/ProfileView.jsx
+++ app/components/ProfileView.jsx
@@ -48,29 +48,29 @@ export default function ProfileView() {
 
   if (loading)
     return (
-      <div className="">
+      <div className="min-h-screen flex justify-center items-center">
         <LoadingSpinner />
       </div>
     );
 
   if (error)
     return (
-      <div className="text-black flex justify-center p-30">Error: {error}</div>
+      <div className="text-black flex justify-center p-10">Error: {error}</div>
     );
 
   if (!userData)
     return (
-      <div className="text-black flex justify-center p-30">
+      <div className="text-black flex justify-center p-10">
         No profile data found.
       </div>
     );
 
   return (
-    <div className="lg:scale-100 lg:pt-10 pt-10 grid justify-center items-center ">
-     <div className="w-full max-w-md px-4">
-
-          <div className="flex w-90  justify-evenly items-center Poppins rounded-3xl backdrop-blur-md backdrop-saturate-150 shadow-lg border border-white/10 relative">
-          <div className="flex justify-center items-center">
+    <div className="pt-10 px-4 flex justify-center items-center">
+      <div className="w-full max-w-md flex flex-col items-center gap-10">
+     
+        <div className="w-full flex justify-evenly items-center Poppins rounded-3xl backdrop-blur-md backdrop-saturate-150 shadow-lg border border-white/10 relative px-4 py-6">
+          <div className="relative justify-start mr-6">
             {userData.profilePics.length > 0 ? (
               <Image
                 src={userData.profilePics[0].url}
@@ -88,12 +88,13 @@ export default function ProfileView() {
             <div className="absolute inset-0 rounded-full border border-cyan-500 blur-sm opacity-30" />
           </div>
 
-          <div className="p-6 grid justify-center">
-            <div className="flex justify-center gap-8 w-max text-white items-center">
-              <div>
-                <h1>{userData.bio?.username ?? "No username"}</h1>
-              </div>
-              <div className="text-xs text-gray-500 flex items-center gap-1">
+          {/* Text Info */}
+          <div className="flex flex-col justify-center text-white">
+            <div className="flex justify-between sm:flex-row items-center gap-3 mb-1">
+              <h1 className="text-base font-semibold">
+                {userData.bio?.username ?? "No username"}
+              </h1>
+               <div className="text-xs flex justify-end text-gray-500  items-center gap-1">
                 <span>📍</span>
                 {userData.bio?.location ? (
                   <div className="grid grid-cols-1">
@@ -105,38 +106,32 @@ export default function ProfileView() {
                   <div>No location</div>
                 )}
               </div>
+        
             </div>
 
-            <div className="flex w-max justify-center text-gray-400 items-center gap-3 text-sm pt-5">
-              <div>
-                {userData.bio?.age ?? "N/A"}
-                <span> yrs,</span>
-              </div>
-              {userData.bio?.job ?? "No job info"}
+            <div className="flex gap-2 text-sm text-gray-400 mb-1">
+              <span>{userData.bio?.age ?? "N/A"} yrs,</span>
+              <span>{userData.bio?.job ?? "No job info"}</span>
             </div>
 
-            <div>
-              <p className="text-gray-400 flex justify-start text-sm text-center mt-1 max-w-xs">
-                " {userData.bio?.bio ?? "No bio info"} "
-              </p>
-            </div>
+            <p className="text-sm text-gray-400 max-w-xs">
+              " {userData.bio?.bio ?? "No bio info"} "
+            </p>
           </div>
         </div>
 
-
         {/* Gallery */}
-        <div className="flex  flex-col items-center justify-center pt-10">
+        <div className="flex w-full pb-15  flex-col items-center justify-center pt-7">
           {userData.photo && userData.photo.length > 0 ? (
             <div
               onClick={handleImageClick}
-              className="cursor-pointer transition-transform hover:scale-105"
+              className="cursor-pointer transition-transform "
             >
               <Image
                 src={userData.photo[0].url}
                 alt="Photo"
                 width={280}
                 height={180}
-              
                 unoptimized
                 className="w-full rounded "
               />
@@ -147,9 +142,7 @@ export default function ProfileView() {
             </div>
           )}
         </div>
-
-      
       </div>
     </div>
   );
-}
+} 
