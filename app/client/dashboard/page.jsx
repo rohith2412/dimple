103181d50c88cdfa309072279c847da295c62890
rohith <rohithra75@gmail.com>
2025-07-17 07:50:36 -0400

    changed ui

--- app/client/dashboard/page.jsx
+++ app/client/dashboard/page.jsx
@@ -1,32 +1,45 @@
-"use client"
+"use client";
+import React from "react";
+import Head from "next/head";
 import Navbar from "../../components/Navbar";
-
-
-import Alluser from '../../components/Alluser'
-import Gear from '../../components/Gear'
-import { Report } from '../../components/Report'
-import React from 'react'
-import BioCheacker from '../../components/BioCheacker'
-import Background from '../../components/Background'
-
+import Alluser from "../../components/Alluser";
+import Gear from "../../components/Gear";
+import { Report } from "../../components/Report";
+import BioCheacker from "../../components/BioCheacker";
+import Background from "../../components/Background";
 
 const Dashboard = () => {
-
-
   return (
-    <div className="">
+    <>
+      <Head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-W4Z4F6JYR2"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-W4Z4F6JYR2');
+            `,
+          }}
+        />
+      </Head>
+
+      <div className="">
         <Background />
         <Navbar />
         <BioCheacker />
-        <div className='pt-10'>
-        <Alluser />
+        <div className="pt-10">
+          <Alluser />
         </div>
-        <div className=''>
+        <div className="">
           <Gear />
         </div>
         <Report />
-    </div>
-  )
-}
+      </div>
+    </>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
