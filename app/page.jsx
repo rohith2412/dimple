7ad113f856b895cee00b8c7b134e1757ad05abc6
Rohith <129610136+rohith2412@users.noreply.github.com>
2025-10-06 23:47:00 -0400

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
 import Landing from "./client/landing/page";
+import Auth from "./client/auth/page"
 
 export default function Home() {
   return (
     <div className="">
       
-      <Landing />
+      {/* <Landing /> */}
+      <Auth />
      
     </div>
   );
